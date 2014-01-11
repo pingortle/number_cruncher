f3063ff9429069bb210c39762779a00684b977ea
Kaleb Lape <pingortle@gmail.com>
2014-01-11 15:49:02 -0600

    Initial commit

---
+++ number_cruncher.rb
@@ -0,0 +1,31 @@
+# Example from: http://www.sitepoint.com/build-sinatra-api-using-tdd-heroku-continuous-integration-travis/
+
+class Integer
+	def factors
+		square_root = self**0.5
+		(1..square_root)
+			.map { |n| [n, self/n] if self/n*n == self }
+			.compact
+			.flatten
+			.sort
+	end
+
+	def prime?
+		self.factors.size == 2
+	end
+end
+
+require 'sinatra'
+require 'json'
+
+get '/:number' do
+	content_type :json
+	number = params[:number].to_i
+	{
+		number: number,
+		factors: number.factors,
+		odd: number.odd?,
+		even: number.even?,
+		prime: number.prime?
+	}.to_json
+end
